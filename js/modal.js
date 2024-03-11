742f4c6b531b7b4078a4ae408985d0ff76e98a23
Yaroslav Litvinenko <jaros.dev@gmail.com>
2024-03-11 08:42:14 +0200

    Last

--- js/modal.js
+++ js/modal.js
@@ -3,11 +3,12 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
